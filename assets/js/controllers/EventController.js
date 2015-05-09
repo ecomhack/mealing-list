6a90507b453923ecb855e73f805bf3160f647dd4
Tom Beckmann <tomjonabc@gmail.com>
2015-05-09 18:51:44 +0200

    Basic infrastructure for server communication

--- assets/js/controllers/EventController.js
+++ assets/js/controllers/EventController.js
@@ -6,7 +6,7 @@ angular.module('mealingList')
 
     var eventId = window.location.search.split('=')[1];
 
-    io.socket.get('/event/' + eventId + '/dishes', function(data, jwres) {
+    io.socket.get('/dish?where={"event":"' + eventId + '"}', function(data, jwres) {
       if (!report('Loading dishes', jwres, true))
         return;
 
@@ -15,12 +15,58 @@ angular.module('mealingList')
       });
     });
 
-    io.socket.on('dish', function(event) {
-      if (event.data.event != eventId)
-        return;
+    function findDishById(dishId) {
+      for (var i = 0; i < $scope.dishes.length; i++) {
+        if ($scope.dishes[i].id == dishId)
+          return $scope.dishes[i];
+      }
+    }
 
+    function findIngredientById(id, dish) {
+      for (var i = 0; i < dish.ingredients; i++) {
+        if (dish.ingredients[i].id == id)
+          return dish.ingredients[i];
+      }
+    }
+
+    io.socket.on('ingredient', function(event) {
       switch (event.verb) {
         case 'created':
+          var dish = findDishById(event.data.dish);
+          if (!dish)
+            return;
+
+          dish.ingredients.push(event.data);
+          break;
+        case 'updated':
+          var dish = findDishById(event.data.dish);
+          if (!dish)
+            return;
+
+          var ingredient = findIngredientById(event.id, dish);
+          var keys = Object.keys(event.data);
+          keys.forEach(function(key) {
+            ingredient[key] = event.data[key];
+          });
+        case 'destroyed':
+          var dish = findDishById(event.data.dish);
+          if (!dish)
+            return;
+
+          var ingredient = findIngredientById(event.id, dish);
+          dish.ingredients.splice(dish.ingredients.indexOf(ingredient), 1);
+          break;
+      }
+
+      $scope.$apply();
+    });
+
+    io.socket.on('dish', function(event) {
+      switch (event.verb) {
+        case 'created':
+          if (event.data.event != eventId)
+            return;
+
           $scope.dishes.push(event.data);
           break;
       }
@@ -46,6 +92,27 @@ angular.module('mealingList')
 
     $scope.selectDish = function(dish) {
        $scope.selectedDish = dish;
+    };
+
+    $scope.createIngredient = function createIngredient() {
+      if (!$scope.selectedDish)
+        throw 'WTF?';
+
+      io.socket.post('/ingredient', {
+        title: $scope.createIngredientInput,
+        dish: $scope.selectedDish.id
+      }, function(data, jwres) {
+        if (!report('Create Ingredient', jwres, true))
+          return;
+
+        $scope.$apply(function() {
+          if (!$scope.selectedDish.ingredients)
+            $scope.selectedDish.ingredients = [];
+
+          $scope.selectedDish.ingredients.push(data);
+          $scope.createIngredientInput = '';
+        });
+      });
     }
   });
 
