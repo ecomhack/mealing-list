bf468eed136e95d19961259a3c81a55685dcea38
Milan Gruner <milangruner@gmail.com>
2015-05-10 02:19:18 +0200

    Created a CSV Crawler for the Food API
    CSV data is in WebApp/js/

---
+++ WebApp/js/megacrawler.js
@@ -0,0 +1,37 @@
+// megacrawler.js
+// (c) 2015 by Milan Gruner
+
+var CRAWL_BEGIN = 73;
+var CRAWL_END = 200;
+var URL = "https://api.foodpairing.com/ingredients/";
+
+function getFoodData(url, callback) {
+	jQuery.ajax({ 
+		headers : {
+			"X-Application-ID": "ec05d5d3",
+			"X-Application-Key": "55c949b646227173b8f99df6cbae8e08"
+		},
+		url: url,
+		type: "GET",
+		dataType: "JSON", 
+		data: {},
+		success: function(data) {
+			callback(data);
+		},
+		xhrField: {
+			withCredentials: false
+		}
+	});
+}
+
+function saveEntry(data) {
+	document.write(data.id+","+data.product+"\n");
+}
+
+function fuckingCrawl() {
+	for(var i = CRAWL_BEGIN; i < CRAWL_END; i++) {
+		getFoodData(URL+i, saveEntry);
+	}
+}
+
+$(fuckingCrawl);
